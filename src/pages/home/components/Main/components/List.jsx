cb0d573958cf8803523d3aa6b43847a795e1ee5c
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-08-30 15:57:08 -0300

    add:Test

--- src/pages/home/components/Main/components/List.jsx
+++ src/pages/home/components/Main/components/List.jsx
@@ -1,17 +1,21 @@
 import usePosts from "../../../../../context/usePosts";
+import Test from "../../../../../Test";
 
 
 const List = () => {
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      <Test />
+    </>
   );
 };
 
